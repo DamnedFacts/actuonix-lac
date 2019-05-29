6e5ada699c1e1980063314ec1987b8b5adc03029
Richard Sarkis <rich@sark.is>
2019-05-29 14:46:35 -0400

    Added python packaging under the name 'actuonix_lac'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='actuonix_lac',
+      version='0.1',
+      description='Python-based controller for Actuonix Linear Actuator Arm (LAC)',
+      url='https://github.com/DamnedFacts/actuonix-lac',
+      author='Jerimiah Daley, and Richard Emile Sarkis',
+      author_email='rich@sark.is',
+      license='MIT',
+      packages=['actuonix_lac'],
+      zip_safe=False)
